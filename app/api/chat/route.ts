7151bea59f8715837971c090fd10ea902c92d8e8
ashutoshsolanki <ashutoshsolanki100100@gmail.com>
2026-05-01 14:19:22 -0500

    fix: remove env file

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,8 +3,11 @@ import { google } from "@ai-sdk/google";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  console.log("KEY:", process.env.GOOGLE_GENERATIVE_AI_API_KEY);
+
   const result = await streamText({
-    model: google("gemini-1.5-flash"),
+    model: google("gemini-2.0-flash"),
     system: "You are ElectAssist, a friendly and neutral assistant that explains the election process, timelines, and voting steps clearly. Use simple language. Be factual and never take political sides.",
     messages,
   });
